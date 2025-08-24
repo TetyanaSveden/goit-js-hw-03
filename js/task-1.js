33a92a9826613c2d7fd1d703bac3a3c1f72e2a55
TetyanaSveden <tan6ka83@yahoo.com>
2025-08-24 21:33:10 +0200

    korr

--- js/task-1.js
+++ js/task-1.js
@@ -2,8 +2,7 @@ function slugify(title) {
     return title
         .toLowerCase()
         .trim()
-        .replace(/\s+/g, "-")
-        ;
+        .replace(/\s+/g, "-");
 };
 console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
 console.log(slugify("English for developer")); // "english-for-developer"
